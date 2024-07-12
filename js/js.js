b09cb9e9225508bd3540fce78b667c18488d5ea3
Roj Beberino <rojaniebeberino0206@gmail.com>
2024-07-12 17:28:57 +0800

    done with the revisions

--- js/js.js
+++ js/js.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
     $('.menu-toggle').click(function(){
         $('.nav-menu').toggleClass('open');
-    })
+    });
+
+
+
+    $('#grid').click(function() {
+        $('.list').css('display', 'none');
+        $('.grid').css('display', 'flex');
+        $('.listing-map').css('width', '35%');
+        $('.list-items').css('display', 'flex');
+    });
+
+    $('#list').click(function() {
+        $('.grid').css('display', 'none');
+        $('.list').css('display', 'block');
+        $('.listing-map').css('width', '35%');
+        $('.list-items').css('display', 'flex');
+    });
+
+    $('#map').click(function() {
+        $('.grid').css('display', 'none');
+        $('.list').css('display', 'none');
+        $('.listing-map').css('width', '100%');
+        $('.list-items').css('display', 'none');
+    });
 
 
 
@@ -15,4 +38,7 @@ $(document).ready(function(){
             navbar.removeClass('sticky');
         }
     });
+
+
+
 });
