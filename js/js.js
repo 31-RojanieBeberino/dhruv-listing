3ba3efcef43e2e2002564da416c02c1513fad149
Roj Beberino <rojaniebeberino0206@gmail.com>
2024-07-16 15:29:25 +0800

    done with the drop down

--- js/js.js
+++ js/js.js
@@ -5,6 +5,28 @@ $(document).ready(function(){
 
 
 
+
+    // $('.prop-type').click(function(){
+    //     $('.prop-list').addClass('active');
+    //     $('.prop-type').addClass('prop-type-active');
+    // });
+
+
+    // $('.prop-type').click(function(event){
+    //     event.stopPropagation(); // Prevent the click event from propagating to the document
+    //     $('.prop-list').addClass('active');
+    //     $('.prop-type').addClass('prop-type-active');
+    // });
+
+    // $(document).click(function(event) {
+    //     if (!$(event.target).closest('.prop-type, .prop-list').length) {
+    //         // Remove the .active and .prop-type-active classes
+    //         $('.prop-list').removeClass('active');
+    //         $('.prop-type').removeClass('prop-type-active');
+    //     }
+    // });
+
+
     $('#grid').click(function() {
         $('.list').css('display', 'none');
         $('.grid').css('display', 'flex');
@@ -41,4 +63,45 @@ $(document).ready(function(){
 
 
 
+});
+
+
+
+// const dropdowns = document.querySelectorAll('.prop-type');
+
+
+// dropdowns.forEach(dropdown => {
+//     const select = dropdown.querySelector('.prop-name');
+//     const chevo = dropdown.querySelector('.chevo');
+//     const menuOpt = dropdown.querySelector('.prop-list');
+
+
+//     select.addEventListener('click', () =>{
+//         menuOpt.classList.toggle('prop-list-active');
+//         chevo.classList.toggle('chevo-active');
+//     })
+// })
+
+
+
+const dropdowns = document.querySelectorAll('.prop-type');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.prop-name');
+    const chevo = dropdown.querySelector('.chevo');
+    const menuOpt = dropdown.querySelector('.prop-list');
+
+    select.addEventListener('click', () => {
+        // Close all active dropdowns
+        dropdowns.forEach(dd => {
+            if (dd !== dropdown) {
+                dd.querySelector('.prop-list').classList.remove('prop-list-active');
+                dd.querySelector('.chevo').classList.remove('chevo-active');
+            }
+        });
+
+        // Toggle the current dropdown
+        menuOpt.classList.toggle('prop-list-active');
+        chevo.classList.toggle('chevo-active');
+    });
 });
